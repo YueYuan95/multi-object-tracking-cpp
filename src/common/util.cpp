7e66561861158e83ba689ffe5ed41654f76fb9d0
Yue Yuan <yyuan@aiit.org.cn>
2019-08-14 23:38:44 +0800

    add tree to graph

--- src/common/util.cpp
+++ src/common/util.cpp
@@ -1,12 +1,62 @@
 #include "util.h"
 
+int preorderTraversal(treeNode tree_node, std::vector<int>& path, 
+                std::vecteor<std::vector<int>>& path_list){
+    
+    path.push_back(tree_node.index);
+    if(tree_node.chirlden.size() == 0){
+        path_list.push(path);
+    }else{
+        for(int i=0; i < tree_node.chirlden.size(); i++){
+            preordeTraversal(tree_node[i], path, path_list);i
+            path.pop_back();
+        }
+    }
+
+}
+
+int backTraversal(treeNode tree_node, treeNode head_node, std::vector<int>& path, 
+                std::vector<std::vector<int>>& path_list, int N){
+    path.push_back(tree_node.index);
+    /*When the depth of the tree is not big than N*/
+    if(tree_node.parent == head_node){
+        path.push_back(tree_node.parent.index, path, path_list);
+        if(path.size() < N){
+            for(int i=N-path.size();i>0;i--){
+                path.push_back(0);
+            }
+        }
+        path_list.push_back(path);
+        return;
+    }
+    /*When this node is a root node*/
+    if(tree_node.parent == null &&  path.size() < N){
+        for(int i=N-path.size(); i > 0; i--){
+            path.push_back(0);
+        }
+        path_list.push_back(path);
+        return;
+
+    }
+    backTraversal(tree_node.parent, head_node, path, path_list);
+}
+
 int TreeToGraph(std::vector<Tree> tree_list, Graph graph){
 
-    for(auto tree : tree_list){
+    std::vector<int> path; 
+    std::vector<std::vector<int>> path_list;
 
-        VexNode temp_node;
-        temp_node.path.clear();
+    for(auto tree : tree_list){
+        
+        //temp_node.path.clear();
         //tmep_node.path.push_back();
+        
+        //preorderTraversal(tree.getHead(),path, path_list);
+        for(auto leaf : tree.leaf_node){
+             path.clear();
+             backTraversal(leaf, path, path_list, tree.getN());
+        }
+        
     }
 }
 
